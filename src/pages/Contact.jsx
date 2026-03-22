ae1d7ab2dab1f00a8f246ffebd59bfbbf951307a
jussinevalainen <jussi.nevalainen89@gmail.com>
2026-03-22 22:08:59 +0200

    test

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -176,7 +176,7 @@ export default function Contact() {
 
               <div className="flex flex-col sm:flex-row gap-4 mt-2">
                 {/* Kuva 1 */}
-                <figure className="w-full sm:w-1/2 cursor-pointer transition-transform duration-300 hover:scale-105">
+                <figure className="w-full sm:w-1/2 cursor-pointer transition-transform duration-300 hover:scale-103">
                   <div
                     className="relative rounded-sm overflow-hidden group"
                     onClick={() => setZoomedImage("/signs.jpg")}
