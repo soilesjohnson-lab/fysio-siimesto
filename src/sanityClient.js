ce37b00630a29a6f37e0697a1acdefb24d642f2d
SideSlideJuicy <jussi.nevalainen89@gmail.com>
2026-04-07 17:52:10 +0300

    FIX: SEO

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'eblm6m06',   // <- tähän oikea ID
-  dataset: 'production',    // <- yleensä production
-  useCdn: false,             // nopeampi, välimuisti sallittu
-  apiVersion: '2026-01-29', // nykyinen päivämäärä tai haluttu versionumero
+  projectId: 'eblm6m06',
+  dataset: 'production',
+  useCdn: false,
+  apiVersion: '2026-01-29',
 });
